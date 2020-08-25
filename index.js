75d0f94312f9116d8ef7c70d335f6ae87681019c
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-25 18:18:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten('Ralph') { kittens.push('Ralph'); return kittens; }
+function destructivelyAppendKitten('Ralph') { return [...kittens, 'Ralph'] }
