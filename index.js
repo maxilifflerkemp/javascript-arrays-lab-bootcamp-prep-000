bf37e153ac1e98254f001fcb71940e275b6b860b
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-25 18:26:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ function destructivelyAppendKitten(Ralph) { kittens.push('Ralph'); return kitten
 function destructivelyPrependKitten(Bob) { kittens.unshift('Bob'); return kittens; }
 
 function destructivelyRemoveLastKitten() { kittens.pop(); return kittens; }
+
+function destructivelyRemoveFirstKitten() { kittens.shift(); return kittens; }
